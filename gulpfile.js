bb65bc9688e9d15b58437c86d6419c567ca1255b
ZhouJihao <249662605@qq.com>
2015-12-16 17:15:37 +0800

    sass build

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ js代码校验（gulp-jshint）
 图片缓存，只有图片替换了才压缩（gulp-cache）
 更改提醒（gulp-notify）*/
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
@@ -25,20 +25,20 @@ var gulp = require('gulp'),
 // Styles任务
 gulp.task('styles', function () {
     //编译sass
-    return gulp.src('stylesheets/main.scss')
+    return gulp.src('stylesheet/*.scss')
         .pipe(sass())
         //添加前缀
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         //保存未压缩文件到我们指定的目录下面
-        .pipe(gulp.dest('stylesheets'))
+        /*.pipe(gulp.dest('stylesheet'))*/
         //给文件添加.min后缀
-        .pipe(rename({
+        /*.pipe(rename({
             suffix: '.min'
-        }))
+        }))*/
         //压缩样式文件
         .pipe(minifycss())
         //输出压缩文件到指定目录
-        .pipe(gulp.dest('assets'))
+        .pipe(gulp.dest('assets/css/'))
         //提醒任务完成
         .pipe(notify({
             message: 'Styles task complete'
@@ -48,7 +48,7 @@ gulp.task('styles', function () {
 // Scripts任务
 gulp.task('scripts', function () {
     //js代码校验
-    return gulp.src('javascripts/*.js')
+    return gulp.src('javascript/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         //js代码合并
@@ -84,7 +84,7 @@ gulp.task('images', function () {
 
 // Default task
 gulp.task('default', function() {
-    gulp.start('scripts');
+    gulp.start('styles','scripts');
 });
 
 // Watch
